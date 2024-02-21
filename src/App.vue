11f7d6601b19c1a65f64ef4634fc8ba1843d81b1
DESKTOP-R1B1MF8\nh <yjkim2@wylie.co.kr>
2024-02-21 14:28:19 +0900

    모달창 상세 작업

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,19 @@
 
   <div v-for="(item, idx) in products" :key="idx">
     <img :src="item.image">
-    <h4 v-on:click="modalOpen = true">{{ item.title }}</h4>
-    <p>{{ item.content }}</p>
+    <h4 v-on:click="modalOpen = true; clickWhat = idx">{{ item.title }}</h4>
     <p>{{ item.price }}원</p>
-    <button type="button" v-on:click="item.report += 1">허위매물신고</button>
-    <p>신고 수: {{ item.report }}</p>
     <hr>
   </div>
 
   <div class="black-bg" v-if="modalOpen">
     <div class="white-bg">
-      <h4>상세페이지</h4>
-      <p>상세페이지 내용</p>
-
+      <h4>{{ products[clickWhat].title }}</h4>
+      <p>{{ products[clickWhat].content }}</p>
+      <p>{{ products[clickWhat].price }}원</p>
+      <p>신고 수: {{ products[clickWhat].report }}</p>
+      <img :src="products[clickWhat].image">
+      <button type="button" v-on:click="products[clickWhat].report += 1">허위매물신고</button>
       <button type="button" v-on:click="modalOpen = false">닫기</button>
     </div>
   </div>
@@ -30,32 +30,10 @@ export default {
   name: 'App',
   data(){// 데이터 보관통
     return {
-      price1 : 60,
-      price2 : 70,
       products: oneromeData,
-      // products_obj: [
-      //   {
-      //     tit: '역삼동 원룸',
-      //     price: 60,
-      //     img: require('./assets/room0.jpg'),
-      //     report: 0,
-      //   },
-      //   {
-      //     tit: '천호동 원룸',
-      //     img: require('./assets/room1.jpg'),
-      //     price: 70,
-      //     report: 0,
-      //   },
-      //   {
-      //     tit: '마포구 원룸',
-      //     img: require('./assets/room2.jpg'),
-      //     price: 80,
-      //     report: 0,
-      //   },
-      // ],
       menu: ['Home', 'Shop', 'About'],
-      blueColor: 'color: blue',
       modalOpen: false,
+      clickWhat: 0,
     }
   },
   components: {
