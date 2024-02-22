74288193091ab579b42960dafdcea41eba7363f9
DESKTOP-R1B1MF8\nh <yjkim2@wylie.co.kr>
2024-02-22 16:04:17 +0900

    인풋 입력 조건문

--- src/TheModal.vue
+++ src/TheModal.vue
@@ -6,6 +6,12 @@
       <p>{{ products[clickWhat].price }}원</p>
       <p>신고 수: {{ products[clickWhat].report }}</p>
       <img :src="products[clickWhat].image">
+      <br>
+      <label for="numMonth">개월 수 </label>
+      <input type="text" id="numMonth" v-model.number="month">
+      <p>{{ month }} 개월 선택 시: {{ products[clickWhat].price + month }}원 </p>
+      <br>
+      <br>
       <button type="button" v-on:click="$emit('ModalReport', '뿡')">허위매물신고</button>
       <button type="button" v-on:click="$emit('ModalClose')">닫기</button>
     </div>
@@ -13,12 +19,27 @@
 </template>
 
 <script>
-    export default {
-        name: 'TheModal',
-        props: {
-            products: Array,
-            clickWhat: Number,
-            modalOpen: Boolean,
+  export default {
+    name: 'TheModal',
+    data(){
+      return {
+        month: 1,
+      }
+    },
+    watch: {
+      month(info){
+        if(typeof info !== 'number'){
+          alert('숫자로 입력해주세요.');
+          this.month = 1;
+        } else if(info >= 13){
+          alert('1 ~ 12 사이의 숫자를 입력해주세요.')
         }
+      }
+    },
+    props: {
+        products: Array,
+        clickWhat: Number,
+        modalOpen: Boolean,
     }
+  }
 </script>
